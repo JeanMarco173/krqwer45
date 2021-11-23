6468684db2e545723d1dad510ab8698a4f45fc46
JeanMarko173 <jeanmarko.13.7@gmail.com>
2021-11-23 11:35:24 -0500

    Agrega TerminarTarea

--- src/App.js
+++ src/App.js
@@ -1,34 +1,56 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 /// Modifica el componente para que se puedan agregar tareas, tachar y destacharlas y error de validacion en el input
 
-class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      tasks: [
-        { id: 1, name: "Sacar la ropa", done: false },
-        { id: 2, name: "Hacer la cama", done: true },
-        { id: 3, name: "Leer un rato", done: false }
-      ],
-      newTask: ''
+export default function App () {
+
+  const [ tasks, setTasks ] = useState([
+    { id: 1, name: "Sacar la ropa", done: false },
+    { id: 2, name: "Hacer la cama", done: true },
+    { id: 3, name: "Leer un rato", done: false }
+  ])
+  const [ newTask, setNewTask ] = useState('')
+  const [ isInvalid, setIsInvalid ] = useState(false)
+
+  const handlePressEnter = e => {
+    e.preventDefault();
+    if(newTask){
+      let auxTasks = [...tasks]
+      const newTaskObj = {
+        id: auxTasks[auxTasks.length - 1].id + 1,
+        name: newTask,
+        done: false
+      }
+      auxTasks.push(newTaskObj)
+      setTasks(auxTasks)
+      setIsInvalid(false)
+      setNewTask('')
+    }else{
+      setIsInvalid(true)
     }
   }
-  render() {
-    return (
-      <div className="wrapper">
-        <div className="list">
-          <h3>Por hacer:</h3>
-          <ul className="todo">
-            {this.state.tasks.map((task, index) => <li key={task.id}>{task.name}</li>)}
-          </ul>
-          <form>
-            <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" value={this.state.newTask} />
-          </form>
-        </div>
-      </div>
-    )
-  }
-}
 
-export default App;
+  const handlePressTask = index => {
+    let auxTasks = [...tasks]
+    auxTasks[index].done = !auxTasks[index].done;
+    setTasks(auxTasks)
+  }
+
+  return (
+    <div className="wrapper">
+      <div className="list">
+        <h3>Por hacer:</h3>
+        <ul className="todo">
+          { tasks.map((task, index) => <li key={task.id} onClick={ () => handlePressTask(index)} className={ task.done ? "done" : null }>{task.name}</li>) }
+        </ul>
+        <form onSubmit={handlePressEnter}>
+          <input type="text" id="new-task"
+            placeholder="Ingresa una tarea y oprime Enter"
+            className={ isInvalid ? "error" : null }
+            value={newTask}
+            onChange={ e => setNewTask(e.target.value) } />
+        </form>
+      </div>
+    </div>
+  )
+}
